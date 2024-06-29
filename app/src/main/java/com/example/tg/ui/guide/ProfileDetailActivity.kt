f2da017d803937a40ec214dee7dc4aae79f15e52
sinjiwon <shinjw0619@gmail.com>
2024-06-29 20:20:24 +0900

    되돌리기 버튼 없이 되돌라감..

---
+++ app/src/main/java/com/example/tg/ui/guide/ProfileDetailActivity.kt
@@ -0,0 +1,26 @@
+package com.example.tg.ui.profile
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tg.databinding.ActivityProfileDetailBinding
+
+class ProfileDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProfileDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityProfileDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val name = intent.getStringExtra("name")
+        val phone = intent.getStringExtra("phone")
+        val email = intent.getStringExtra("email")
+        val location = intent.getStringExtra("location")
+
+        binding.textName.text = name
+        binding.textPhone.text = phone
+        binding.textEmail.text = email
+        binding.textLocation.text = location
+    }
+}
