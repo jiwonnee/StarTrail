fd6689db0ea5ff3e0712220bdc64b0e82774a124
geon314159 <geon314159@kaist.ac.kr>
2024-07-01 17:35:50 +0900

    Add tap click on 1st tap
    3rd tap 추가 예정

--- app/src/main/java/com/example/tg/ui/guide/GuideFragment.kt
+++ app/src/main/java/com/example/tg/ui/guide/GuideFragment.kt
@@ -45,7 +45,7 @@ class GuideFragment : Fragment() {
         return root
     }
 
-override fun onDestroyView() {
+    override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
@@ -81,7 +81,7 @@ override fun onDestroyView() {
             val context = holder.profile.context
             val imageResId = context.resources.getIdentifier(contact.imageName, "drawable", context.packageName)
 
-            holder.profile.setOnClickListener {
+            holder.itemView.setOnClickListener {
                 val intent = Intent(context, ProfileDetailActivity::class.java).apply {
                     putExtra("name", contact.name)
                     putExtra("phone", contact.phone)
