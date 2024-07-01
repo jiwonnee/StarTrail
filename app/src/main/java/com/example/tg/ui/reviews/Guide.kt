91c1866dba14b9954d870eab82b70aaf82facc54
geon314159 <geon314159@kaist.ac.kr>
2024-07-01 17:49:44 +0900

    Commit problem solved
    - Add guide.kt

---
+++ app/src/main/java/com/example/tg/ui/reviews/Guide.kt
@@ -0,0 +1,11 @@
+package com.example.tg.ui.reviews
+
+data class Guide(
+    val name: String,
+    val description: String,
+    val rating: Double,
+    val phone: String,
+    val email: String,
+    val location: String,
+    val imageName: String
+)
